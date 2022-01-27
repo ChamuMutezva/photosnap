6a129e8bef1b2d7c99ec7ce92391bff883154d11
Chamunorwa Mutezva <ckmutezva@gmail.com>
2022-01-27 06:13:23 +0200

    products table using flex

--- src/pages/pricing/Plans.js
+++ src/pages/pricing/Plans.js
@@ -1,56 +1,62 @@
 import React from 'react'
+import Products from './Products'
 
 function Plans(props) {
     console.log(props)
     return (
-        <div className="cards container">
-            <ul className="card-list">
-                <li className="card-item container">
-                    <h3>Basic</h3>
-                    <p className="card-content">
-                        Includes basic usage of our platform.Recommended for new and aspiring photographers.
-                    </p>
-                    <p className="card-cost">
-                        <span className="monthly-yearly-cost" aria-live="polite">
-                            {props.option === "yearly" ? "$190.00 per year" : "$19.00 per month"}
-                        </span>
-                    </p>
-                    <button>
-                        Pick Plan
-                    </button>
-                </li>
-                <li className="card-item container">
-                    <h3>Pro</h3>
-                    <p className="card-content">
-                        More advanced features available.Recommended for photography veterans and professionals
-                    </p>
-                    <p className="card-cost">
-                        <span className="monthly-yearly-cost" aria-live="polite">
-                            {props.option === "yearly" ? "$390.00 per year" : "$39.00 per month"}
-                        </span>
-                    </p>
-                    <button>
-                        Pick Plan
-                    </button>
-                </li>
-                <li className="card-item container">
-                    <h3>Business</h3>
-                    <p className="card-content">
-                        Additional features available such as more detailed metrics.Recommended for business owners.
-                    </p>
-                    <p className="card-cost">
-                        <span className="monthly-yearly-cost" aria-live="polite">
-                            {props.option === "yearly" ? "$990.00 per year" : "$99.00 per month"}
-                        </span>
-                    </p>
-                    <button>
-                        Pick Plan
-                    </button>
-                </li>
+        <>
+            <div className="cards container">
+                <ul className="card-list">
+                    <li className="card-item container">
+                        <h3>Basic</h3>
+                        <p className="card-content">
+                            Includes basic usage of our platform.Recommended for new and aspiring photographers.
+                        </p>
+                        <p className="card-cost">
+                            <span className="monthly-yearly-cost" aria-live="polite">
+                                {props.option === "yearly" ? "$190.00 per year" : "$19.00 per month"}
+                            </span>
+                        </p>
+                        <button>
+                            Pick Plan
+                        </button>
+                    </li>
+                    <li className="card-item container">
+                        <h3>Pro</h3>
+                        <p className="card-content">
+                            More advanced features available.Recommended for photography veterans and professionals
+                        </p>
+                        <p className="card-cost">
+                            <span className="monthly-yearly-cost" aria-live="polite">
+                                {props.option === "yearly" ? "$390.00 per year" : "$39.00 per month"}
+                            </span>
+                        </p>
+                        <button>
+                            Pick Plan
+                        </button>
+                    </li>
+                    <li className="card-item container">
+                        <h3>Business</h3>
+                        <p className="card-content">
+                            Additional features available such as more detailed metrics.Recommended for business owners.
+                        </p>
+                        <p className="card-cost">
+                            <span className="monthly-yearly-cost" aria-live="polite">
+                                {props.option === "yearly" ? "$990.00 per year" : "$99.00 per month"}
+                            </span>
+                        </p>
+                        <button>
+                            Pick Plan
+                        </button>
+                    </li>
 
-            </ul>
+                </ul>
 
-        </div>
+            </div>
+            <div className="producs">
+                <Products />
+            </div>
+        </>
     )
 }
 
---
+++ src/pages/pricing/Products.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useContext } from 'react'
+import { DataContext } from '../context/Context'
+import check from '../../assets/pricing/desktop/check.svg'
+
+function Products() {
+    const { pricing } = useContext(DataContext)
+    console.log(pricing.features)
+    return (
+        <div className="features">
+            <div className="title-container container">
+                <h2 className="features-title">The Features</h2>
+                <p className="flex feature-content">
+                    <span className="feature-content-title">basic</span>
+                    <span className="feature-content-title">pro</span>
+                    <span className="feature-content-title">business</span>
+                </p>
+            </div>
+            {pricing && pricing.features.map(elm => <div key={elm.name} className="container product-container">
+                <h3 className="product-title">{elm.title}</h3>
+                <ul className="product-list">
+                    <li className="product-list-item">
+                        <p>basic {elm.basic} </p>
+                        <div className="product-status"> <img src={elm.basic ? check : ""} alt="" /></div>
+                    </li>
+                    <li className="product-list-item">
+                        <p>pro</p>
+                        <div className="product-status"> <img src={elm.pro ? check : ""} alt="" /></div>
+                    </li>
+                    <li className="product-list-item">
+                        <p>business</p>
+                        <div className="product-status"> <img src={elm.business ? check : ""} alt="" /></div>
+                    </li>
+                </ul>
+            </div>
+            )}
+        </div>
+    )
+}
+
+export default Products;
