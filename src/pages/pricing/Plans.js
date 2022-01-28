1d6e3d1467cf473a6ecac8f70057dea630eb0167
Chamunorwa Mutezva <ckmutezva@gmail.com>
2022-01-28 05:20:01 +0200

    card title and spans for amount classes and display

--- src/pages/pricing/Plans.js
+++ src/pages/pricing/Plans.js
@@ -8,13 +8,16 @@ function Plans(props) {
             <div className="cards container">
                 <ul className="card-list">
                     <li className="card-item container">
-                        <h3>Basic</h3>
+                        <h3 className="card-item-title">Basic</h3>
                         <p className="card-content">
                             Includes basic usage of our platform.Recommended for new and aspiring photographers.
                         </p>
-                        <p className="card-cost">
-                            <span className="monthly-yearly-cost" aria-live="polite">
-                                {props.option === "yearly" ? "$190.00 per year" : "$19.00 per month"}
+                        <p className="card-cost" aria-live="polite">
+                            <span className="monthly-yearly-cost" >
+                                {props.option === "yearly" ? "$190.00 " : "$19.00 "}
+                            </span>
+                            <span className="duration-term">
+                                {props.option === "yearly" ? " per year" : " per month"}
                             </span>
                         </p>
                         <button>
@@ -22,13 +25,16 @@ function Plans(props) {
                         </button>
                     </li>
                     <li className="card-item container">
-                        <h3>Pro</h3>
+                        <h3 className="card-item-title">Pro</h3>
                         <p className="card-content">
                             More advanced features available.Recommended for photography veterans and professionals
                         </p>
                         <p className="card-cost">
                             <span className="monthly-yearly-cost" aria-live="polite">
-                                {props.option === "yearly" ? "$390.00 per year" : "$39.00 per month"}
+                                {props.option === "yearly" ? "$390.00 " : "$39.00 "}
+                            </span>
+                            <span className="duration-term">
+                                {props.option === "yearly" ? " per year" : " per month"}
                             </span>
                         </p>
                         <button>
@@ -36,13 +42,16 @@ function Plans(props) {
                         </button>
                     </li>
                     <li className="card-item container">
-                        <h3>Business</h3>
+                        <h3 className="card-item-title">Business</h3>
                         <p className="card-content">
                             Additional features available such as more detailed metrics.Recommended for business owners.
                         </p>
                         <p className="card-cost">
                             <span className="monthly-yearly-cost" aria-live="polite">
-                                {props.option === "yearly" ? "$990.00 per year" : "$99.00 per month"}
+                                {props.option === "yearly" ? "$990.00 " : "$99.00 "}
+                            </span>
+                            <span className="duration-term">
+                                {props.option === "yearly" ? " per year" : " per month"}
                             </span>
                         </p>
                         <button>
