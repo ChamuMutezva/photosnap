361985ab628928e48809f46e63aa412d2d393833
Chamunorwa Mutezva <ckmutezva@gmail.com>
2022-01-30 09:23:42 +0200

    adding animation to the stories component

--- src/pages/shared/Hero.js
+++ src/pages/shared/Hero.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/ScrollTrigger';
 
-function Hero(props) {   
+function Hero(props) {
+    gsap.registerPlugin(ScrollTrigger)
+    const heroRef = useRef(null)
+
+    useEffect(() => {
+        gsap.from(heroRef.current, {
+            duration: 1,
+            autoAlpha: 0,
+            ease: "none",
+            delay: 1,
+            scrollTrigger: {
+                trigger: heroRef.current,
+                start: "top center+=100",
+                toggleActions: "play none none reverse"
+            }
+        })
+    }, [heroRef]);
+
 
     return (
-        <div className="hero">
+        <div ref={heroRef} className="hero">
             <picture>
                 <source media="(min-width: 67.5rem)" srcSet={props.desktop} />
                 <source media="(min-width: 43.125rem)" srcSet={props.tablet} />
--- src/pages/shared/Story.js
+++ src/pages/shared/Story.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/ScrollTrigger';
 import LinkArrow from './LinkArrow'
 
 function Story(props) {
+    gsap.registerPlugin(ScrollTrigger)
+    const storyRef = useRef(null)
+
+    useEffect(() => {
+        gsap.from([storyRef.current], {
+            duration: 1,
+            autoAlpha: 0,
+            ease: "none",
+            delay: 1,
+            scrollTrigger: {
+                trigger: storyRef.current,
+                start: "top center+=100",
+                stagger: 0.5,
+                toggleActions: "play none none reverse"
+            }
+        })
+    }, [storyRef]);
+
     return (
-        <div className="story">
+        <div ref={storyRef} className="story">
             <picture>
-              {/*  <source media="(min-width: 67.5rem)" srcSet={props.desktop} /> */}
                 <source media="(min-width: 43.125rem)" srcSet={props.tablet} />
                 <img src={props.mobile} alt="" className="hero-img" />
             </picture>
